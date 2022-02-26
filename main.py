26171ef2dc1ce13021459c6a3b05d76a189f209a
Guilherme Vieira <unifggoncalves@fei.edu.br>
2022-02-25 21:01:22 -0300

    rgbToGray

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-import rgbToGray 
+from rgbToGray import * 
+import cv2
+
+
+if __name__ == '__main__':
+    image1 = cv2.imread("lena.jpg")
+    image2 = cv2.imread("lena.jpg")
+
+    grayImg1 = rgbToGray(image1)
+    grayImg2 = rgbToGray(image2)
+
+    cv2.namedWindow("gray image",2)
+    cv2.imshow("gray image",grayImg1)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows
--- rgbToGray.py
+++ rgbToGray.py
@@ -1,4 +1,7 @@
+import cv2
+import numpy as np
+from skimage import color
+
 def rgbToGray(image):
-    R, G, B = img[:, :, 0], img[:, :, 1], img[:, :, 2]
-    img = 0.2989 * R + 0.5870 * G + 0.1140 * B
+    image = color.rgb2gray(image)
     return image
