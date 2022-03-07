1af9d86c051b8688f201c542b219b279d090ddee
Guilherme Vieira <unifggoncalves@fei.edu.br>
2022-03-07 12:43:23 -0300

    convolução

--- gaussianFilter.py
+++ gaussianFilter.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.ndimage.filters import convolve
 
 def gaussianFilter(sigma):
     size = 2*np.ceil(3*sigma)+1 
@@ -6,4 +7,7 @@ def gaussianFilter(sigma):
     g = np.exp(-((x**2 + y**2)/(2.0*sigma**2))) / (2*np.pi*sigma**2)
     return g/g.sum()
 
+def convolucao(image, gaussianfilter):
+    image = convolve(image, gaussianfilter)
+    return image
 
--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-from rgbToGray import * 
+from rgbToGray import *
+from gaussianFilter import *
 import cv2
+
 
 
 if __name__ == '__main__':
     image1 = cv2.imread("lena.jpg")
     image2 = cv2.imread("lena.jpg")
+    sigma = 1.6
+    numintervalos = 3
 
     grayImg1 = rgbToGray(image1)
     grayImg2 = rgbToGray(image2)
 
+    filtroGaussiano = gaussianFilter(sigma)
+    imgGaussiana = convolucao(grayImg1, filtroGaussiano)
+
     cv2.namedWindow("gray image",2)
-    cv2.imshow("gray image",grayImg1)
+    cv2.imshow("gray image",imgGaussiana)
     cv2.waitKey(0)
     cv2.destroyAllWindows
